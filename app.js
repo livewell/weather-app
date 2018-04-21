fbb2e0bd2b78726de96655c3c0ac323de4fe11d6
Kory <koryqle@gmail.com>
2018-04-20 17:23:59 -0700

    ...

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const request = require('request');
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lamboard%20street%20philadelphia',
     json: true
-}, function(error,response,body){
-    console.log(body);
+}, (error,response,body) => {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
 
 console.log("im first");
